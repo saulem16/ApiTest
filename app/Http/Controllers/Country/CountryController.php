a291150fd4f7399cc5f3aead50774648ce99982d
Saule <saule.turmanova@gmail.com>
2021-02-03 21:34:31 +0500

    test_project

---
+++ app/Http/Controllers/Country/CountryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Country;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Models\CountryModel;
+
+class CountryController extends Controller
+{
+    public function country(){
+        return response()->json(CountryModel::get(),200);
+    }
+
+    public function countryById($id){
+        return response()->json(CountryModel::find($id),200);
+    }
+
+    public function countrySave(Request $req){
+        $country=CountryModel::create($req->all());
+        return response()->json($country,201);
+    }
+
+    public function countryEdit(Request $req,CountryModel $country){
+        $country->update($req->all());
+        return response()->json($country,201);
+    }
+
+}
---
+++ app/Models/Models/CountryModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CountryModel extends Model
+{
+    protected $table="country_lang";
+    public $timestamps=false;
+    protected $fillable=[
+        'id',
+        'alias',
+        'name',
+        'name_en'
+    ];
+
+}
